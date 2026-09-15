d294acf36ef4c1eef80106c982df7324ebfc6f1f
zato55 <fbboytr@gmail.com>
2026-09-15 19:06:02 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 // ZATOI AI SERVICE WORKER V14.24
-const CACHE_NAME = "zatoi-ai-v14-24";
+const CACHE_NAME = "zatoi-ai-v14-27-1";
 const APP_FILES = ["./", "./index.html",
-  "./style.css", "./manifest.json", "./icon-192.png", "./icon-512.png", "./robots.txt", "./sitemap.xml", "./llms.txt", "./hakkinda/", "./ozellikler/"];
+  "./style.css?v=14.27.1", "./manifest.json", "./icon-192.png", "./icon-512.png", "./robots.txt", "./sitemap.xml", "./llms.txt", "./hakkinda/", "./ozellikler/"];
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => Promise.allSettled(
     APP_FILES.map(file => cache.add(new Request(file, { cache: "reload" })))
